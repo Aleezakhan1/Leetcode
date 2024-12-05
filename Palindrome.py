a16058fbeacfdb7f40e01b0adbcc323f4207979a
Aleezakhan1 <kaliza957@gmail.com>
2024-12-05 13:04:40 +0300

    palindrome problem

--- Palindrome.py
+++ Palindrome.py
@@ -15,14 +15,13 @@ else:
 # Using Iterative Method
 
 def isPalindrome(str):
-    # Run loop from 0 to len/2
+
     new_str = int(len(str)/2)
     for i in range(0, new_str):
         if str[i] != str[len(str)-i-1]:
             return False
     return True
 
-# main function
 s = '121'
 ans = isPalindrome(s)
 
