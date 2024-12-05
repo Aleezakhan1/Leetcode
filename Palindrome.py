800c27ad0ff36fb592ceccf0511518950439c16d
Aleezakhan1 <kaliza957@gmail.com>
2024-12-05 13:03:22 +0300

    palindrome problem

---
+++ Palindrome.py
@@ -0,0 +1,35 @@
+# Native Approach (just reverse the string)
+
+def ispalindrome (str):
+  original_string = str(str)
+  return original_string == original_string[::-1]
+
+str = 121
+str_1 = ispalindrome(str)
+
+if str:
+  print('yes')
+else:
+  print('no')
+
+# Using Iterative Method
+
+def isPalindrome(str):
+    # Run loop from 0 to len/2
+    new_str = int(len(str)/2)
+    for i in range(0, new_str):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+
+# main function
+s = '121'
+ans = isPalindrome(s)
+
+if (ans):
+    print("Yes")
+else:
+    print("No")
+
+
+
